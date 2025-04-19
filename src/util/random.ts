ebc854a566c93aeba6b6ddb23a004daab5c4960f
Carson Crow <carsoncrow@proton.me>
2025-04-19 13:34:02 -0500

    Convert random util to ts

--- src/util/random.ts
+++ src/util/random.ts
@@ -1,6 +1,4 @@
 /** Returns a random item from the given non-empty list. */
-const sample = (list) => {
+export function sample<T>(list: T[]): T {
   return list[Math.floor(Math.random() * list.length)];
 }
-
-module.exports = randomChoice;
