63e8e1372d908b51050c2f360121a169c79101f2
Carson Crow <carsoncrow@proton.me>
2025-04-20 11:15:54 -0500

    Chain gen function

--- src/MarkovCoil.ts
+++ src/MarkovCoil.ts
@@ -1,3 +1,4 @@
+import { sample } from "./util/random";
 
 interface MarkovOptions {
   depth?: number;
@@ -5,61 +6,96 @@ interface MarkovOptions {
 }
 
 interface MarkovVocab {
-  idToToken: string[];
-  tokenToId: Map<string, number>;
+  tokens: string[];
+  tokenIndex: Map<string, number>;
 }
+
+type MarkovChain = Map<string, Map<number, number>>;
 
 const defaultOptions: Required<MarkovOptions> = {
   depth: 3,
   includeSpecialChars: false,
-}
+};
 
 export default class MarkovCoil {
-  private options: Required<MarkovOptions>;
+  options: Required<MarkovOptions>;
   vocab: MarkovVocab = {
-    idToToken: [],
-    tokenToId: new Map<string, number>(),
+    tokens: [],
+    tokenIndex: new Map<string, number>(),
   };
-  chain = new Map<string, number>();
-  
+  chain: MarkovChain = new Map<string, Map<number, number>>();
 
   /**
    * Create new text suggestion Markovinator from seed text.
-   * @param {string} corpus The text seed.
+   * @param {string|string[]} corpus Accepts either a string (which will use default tokenizer with options), or a custom token list.
    * @param {MarkovCoilOptions=} options Options for text suggestion.
    * @param options.includeSpecialChars Excludes non-alphanumeric characters not typical in English text.
    */
-  constructor (corpus: string, options?: MarkovOptions) {
-    this.options = { ...defaultOptions, ...options }
+  constructor(corpus: string | string[], options?: MarkovOptions) {
+    this.options = { ...defaultOptions, ...options };
     this.generate(corpus);
   }
 
   tokenize(text: string) {
-    const trimmed = text.replace(/\s+/g, ' ').trim().toLowerCase();
-    return trimmed.split(' ');
+    const trimmed = text.replace(/\s+/g, " ").trim().toLowerCase();
+    return trimmed.split(" ");
   }
 
   addTokenToVocab(token: string) {
-    const id = this.vocab.tokenToId.get(token);
+    const id = this.vocab.tokenIndex.get(token);
     if (id === undefined) {
-      this.vocab.idToToken.push(token);
-      this.vocab.tokenToId.set(token, this.vocab.idToToken.length - 1);
+      this.vocab.tokens.push(token);
+      this.vocab.tokenIndex.set(token, this.vocab.tokens.length - 1);
     }
   }
 
-  generate(corpus: string) {
-    const tokens = this.tokenize(corpus)
-    tokens.forEach((token, index) => {
-      this.addTokenToVocab(token)
-      const context = tokens.slice(index - this.options.depth + 1, index);
-    })
+  getStateKey(tokens: string[]) {
+    return tokens
+      .map((token) => this.vocab.tokenIndex.get(token))
+      .join("-");
   }
 
-  nextFor(context: string|string[], weighted: boolean = true) {
+  addToChain(state: string[], nextToken: string) {
+    if (!state || nextToken === undefined) return;
+    for (let i = 0; i < state.length; i += 1) {
+      const stateKey = this.getStateKey(state.slice(0, i));
+
+      if (!this.chain.has(stateKey)) {
+        this.chain.set(stateKey, new Map<number, number>());
+      }
+
+      const nextTokenIndex = this.vocab.tokenIndex.get(nextToken)!;
+      const nextMap = this.chain.get(stateKey)!;
+      const nextWeight = nextMap.get(nextTokenIndex);
+      nextMap.set(nextTokenIndex, (nextWeight || 0) + 1)
+    }
+  }
+
+  generate(corpus: string | string[]) {
+    let tokens: string[];
+    if (typeof corpus === "string") {
+      tokens = this.tokenize(corpus);
+    } else {
+      tokens = corpus;
+    }
+
+    tokens.forEach(token => {
+      this.addTokenToVocab(token);
+    })
+
+    for (let i = 0; i < tokens.length; i += 1) {
+      const state = tokens.slice(i, i + this.options.depth);
+      const nextToken = tokens[i + this.options.depth];
+      this.addToChain(state, nextToken)
+    }
+  }
+
+  nextFor(context: string | string[], weighted: boolean = true) {
     if (typeof context === "string") {
       context = this.tokenize(context);
     }
+    if (context.length === 0) {
+      return sample(this.vocab.tokens);
+    }
   }
-
-  
 }
--- test/MarkovCoil.test.ts
+++ test/MarkovCoil.test.ts
@@ -24,13 +24,36 @@ describe("Markov Coil", () => {
   describe("token vocabulary", () => {
     it("generates correctly", () => {
       const mc = new MarkovCoil("The quick brown fox and the quick Brown Dog");
-      const { tokenToId, idToToken } = mc.vocab;
-      expect(idToToken).toEqual(['the', 'quick', 'brown', 'fox', 'and', 'dog'])
-      expect(tokenToId.size).toBe(6);
-      expect(tokenToId.get("the")).toBe(0);
-      expect(tokenToId.get("and")).toBe(4);
+      const { tokenIndex, tokens } = mc.vocab;
+      expect(tokens).toEqual(['the', 'quick', 'brown', 'fox', 'and', 'dog'])
+      expect(tokenIndex.size).toBe(6);
+      expect(tokenIndex.get("the")).toBe(0);
+      expect(tokenIndex.get("and")).toBe(4);
     });
   });
+
+  /**
+   * Depth 0, the key is the word and the value is the weight
+   * Depth 1, "the" => ("quick" => 1)
+   * 
+   */
+
+  describe.only("markov chain", () => {
+    it("generates correctly for depth=1", () => {
+      const mc = new MarkovCoil("the quick fox and the dog".split(" "), { depth: 1 });
+      const expectedChain = new Map([
+        ["0", new Map([["1", 1], ["4", 1]])],
+        ["1", new Map([["2", 1]])],
+        ["2", new Map([["3", 1]])],
+        ["3", new Map([["0", 1]])],
+      ]);
+      expect(mc.chain).toEqual(expectedChain)
+    })
+
+    it("generates correctly for depth=2", () => {
+
+    })
+  })
 
   describe("text suggestion", () => {
     const mc = new MarkovCoil("The quick brown fox and the quick Brown Dog");
