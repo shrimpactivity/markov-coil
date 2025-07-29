41a1408238e9ead1118691ef19569718c2098b29
Carson Crow <carson.crow@texasbar.com>
2025-07-29 12:30:27 -0500

    Update addToChain function

--- src/MarkovCoil.ts
+++ src/MarkovCoil.ts
@@ -18,7 +18,7 @@ import { sample } from "./util/random";
  *  }
  * Vocab:
  *  index -> token: [token1, token2, token3]
- *  token -> index: { token1: index1 }
+ *  token -> index: { token1: [ index, weight ] }
  * 
  */
 
@@ -29,7 +29,7 @@ interface MarkovOptions {
 
 interface MarkovVocab {
   tokens: string[];
-  tokenIndex: Map<string, number>;
+  tokenIndexesAndWeights: Map<string, number[]>;
 }
 
 // Map indexed state key (eg. "0-1-2") to next token index and weight (eg. 3 => 1)
@@ -44,7 +44,7 @@ export default class MarkovCoil {
   options: Required<MarkovOptions>;
   vocab: MarkovVocab = {
     tokens: [],
-    tokenIndex: new Map<string, number>(),
+    tokenIndexesAndWeights: new Map<string, number[]>(),
   };
   chain: MarkovChain = new Map<string, Map<number, number>>();
 
@@ -59,35 +59,63 @@ export default class MarkovCoil {
     this.generate(corpus);
   }
 
-  addTokenToVocab(token: string) {
-    const id = this.vocab.tokenIndex.get(token);
-    if (id === undefined) {
-      this.vocab.tokens.push(token);
-      this.vocab.tokenIndex.set(token, this.vocab.tokens.length - 1);
+  getTokenIndex(token: string) {
+    const indexAndWeight = this.vocab.tokenIndexesAndWeights.get(token)
+    if (indexAndWeight) {
+      return indexAndWeight[1]
     }
+    return undefined
+  }
+
+  getTokenWeight(token: string) {
+    const indexAndWeight = this.vocab.tokenIndexesAndWeights.get(token)
+    if (indexAndWeight) {
+      return indexAndWeight[1]
+    }
+    return 0
+  }
+
+  addTokenToVocab(token: string) {
+    const index = this.getTokenIndex(token);
+    const newIndexAndWeight = [index!, this.getTokenWeight(token) + 1]
+    if (index === undefined) {
+      this.vocab.tokens.push(token);
+      newIndexAndWeight[0] = this.vocab.tokens.length - 1
+    }
+    this.vocab.tokenIndexesAndWeights.set(token, newIndexAndWeight);
   }
 
   getStateKey(tokens: string[]) {
     if (!tokens.length) return "";
     return tokens
-      .map((token) => this.vocab.tokenIndex.get(token))
+      .map((token) => this.getTokenIndex(token))
       .join("-");
   }
 
-  addToChain(state: string[], nextToken: string) {
-    if (!state || nextToken === undefined) return;
-    for (let i = 0; i < state.length; i += 1) {
-      const stateKey = this.getStateKey(state.slice(0, i));
+  incrementWeightFor(stateKey: string, tokenIndex: number) {
+    const indexAndWeight = this.chain.get(stateKey);
+    if (!indexAndWeight) return;
 
-      if (!this.chain.has(stateKey)) {
-        this.chain.set(stateKey, new Map<number, number>());
-      }
+    const currentWeight = indexAndWeight.get(tokenIndex) || 0
+    indexAndWeight.set(tokenIndex, currentWeight + 1)
+  }
 
-      const nextTokenIndex = this.vocab.tokenIndex.get(nextToken)!;
-      const nextMap = this.chain.get(stateKey)!;
-      const nextWeight = nextMap.get(nextTokenIndex);
-      nextMap.set(nextTokenIndex, (nextWeight || 0) + 1)
+  addToChain(state: string[]) {
+    if (state.length === 1) {
+      return
     }
+
+    const prevState = state.slice(0, state.length - 1);
+    const stateKey = this.getStateKey(prevState)
+    const next = state[state.length - 1];
+    const nextIndex = this.getTokenIndex(next)!;
+
+    if (!(this.chain.has(stateKey))) {
+      this.chain.set(stateKey, new Map<number, number>([[nextIndex, 0]]));
+    }
+    
+    this.incrementWeightFor(stateKey, nextIndex)
+    this.addToChain(prevState);
   }
 
   generate(tokens: string[]) {
@@ -96,9 +124,8 @@ export default class MarkovCoil {
     })
 
     for (let i = 0; i < tokens.length; i += 1) {
-      const state = tokens.slice(i, i + this.options.depth);
-      const nextToken = tokens[i + this.options.depth];
-      this.addToChain(state, nextToken)
+      const state = tokens.slice(i, i + this.options.depth + 1);
+      this.addToChain(state)
     }
   }
 
