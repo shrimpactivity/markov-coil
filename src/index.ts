dc1b4bb8e67bebe15a7dcd19a9af07f01de0047b
Carson Crow <carson.crow@texasbar.com>
2025-07-30 10:38:49 -0500

    Add exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import MarkovCoil from "./MarkovCoil"
-import { tokenize } from "./tokenize"
-const tokens = tokenize("the quick fox and the quick dog fox");
-const markov = new MarkovCoil(tokens);
-
-markov.prettyPrint();
+export { MarkovCoil } from "./MarkovCoil";
+export { tokenize } from "./tokenize";
 
